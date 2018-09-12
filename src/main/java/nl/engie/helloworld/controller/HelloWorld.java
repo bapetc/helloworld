402627a945423a8cafe705ecebd339f63c6f716f
peeters <bart@techniconsulting.be>
2018-09-12 14:02:33 +0200

    add

---
+++ src/main/java/nl/engie/helloworld/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package nl.engie.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+
+}
